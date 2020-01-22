ce8cf6b18f12820204889690abd46b176741ba87
Chase Brown <cb216318@ohio.edu>
2020-01-22 18:50:15 -0500

    Hello Git Program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Chase Brown
+ *        Date: 1/22/20
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
